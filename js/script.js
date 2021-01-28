78644d35934e36976e2d8a5757d51d5cca575aeb
To My <myfragithub@gmail.com>
2021-01-28 18:44:50 +0700

    some updates

--- js/script.js
+++ js/script.js
@@ -26,3 +26,51 @@ nav_links.addEventListener('click', (e) => {
         }
     }
 });
+
+
+// Image Gallery
+const images_gallery = document.querySelectorAll('.gallery-wrapper .gallery .image');
+
+images_gallery.forEach((image_wrap) => {
+    image_wrap.style.height = image_wrap.clientWidth + 'px';
+    image_wrap.querySelector('img').style.height = image_wrap.clientWidth + 'px';
+});
+
+
+// Waktu Mundur
+const countDownDate = new Date('4 February 2021 09:00:00').getTime();
+
+setInterval(() => {
+    let currentDate   = new Date().getTime();
+    let distance = countDownDate - currentDate;
+
+    let days    = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours   = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const jam_mundur_wrapper = document.querySelector('.jam-mundur-wrapper');
+
+    jam_mundur_wrapper.innerHTML = `<div class="waktu">
+                                        <span class="number" id="number_days">${days}</span>
+                                        <span class="text">Hari</span>
+                                    </div>
+                                    <div class="waktu">
+                                        <span class="number" id="number_hours">${hours}</span>
+                                        <span class="text">Jam</span>
+                                    </div>
+                                    <div class="waktu">
+                                        <span class="number" id="number_minutes">${minutes}</span>
+                                        <span class="text">Menit</span>
+                                    </div>
+                                    <div class="waktu">
+                                        <span class="number" id="number_seconds">${seconds}</span>
+                                        <span class="text">Detik</span>
+                                    </div>`;
+        
+        if( distance < 0 ) {
+            jam_mundur_wrapper.innerHTML = '';
+        }
+
+}, 1000);
+
